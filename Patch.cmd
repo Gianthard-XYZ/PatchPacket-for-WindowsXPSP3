95c1421b5ca93ff51978d81371eabc0c1825c147
LUO Zhen-huan <i@luozhenhuan.im>
2019-05-19 17:08:05 +0800

    首次更新

---
+++ Patch.cmd
@@ -0,0 +1,305 @@
+@echo off
+echo 即将开始安装Windows XP SP3后补丁合集，安装过程中请勿关闭本窗口。
+echo 正在安装 Windows XP 安全更新程序 (KB898461)(第 1 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB898461-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB950762)(第 2 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB950762-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB951376)(第 3 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB951376-v2-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB951978)(第 4 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB951978-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB950974)(第 5 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB950974-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB946648)(第 6 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB946648-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB952287)(第 7 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB952287-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB952954)(第 8 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB952954-x86-CHS.exe /U /Q /Z
+echo 正在安装 Internet Explorer 8 (第 9 个更新，共 140 个)
+start /wait .\Patch\IE8-WindowsXP-x86-CHS.exe /quiet /update-no /norestart
+echo 正在安装 Windows XP 安全更新程序 (KB959426)(第 10 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB959426-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB960803)(第 11 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB960803-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB923561)(第 12 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB923561-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB952004)(第 13 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB952004-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB956572)(第 14 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB956572-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB973540)(第 15 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-WindowsMedia-KB973540-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB960859)(第 16 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB960859-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB971657)(第 17 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB971657-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB973507)(第 18 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB973507-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB973815)(第 19 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB973815-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB973869)(第 20 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB973869-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB956844)(第 21 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB956844-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB969059)(第 22 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB969059-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB974112)(第 23 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB974112-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB974571)(第 24 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB974571-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB975025)(第 25 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB975025-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB954155)(第 26 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-WindowsMedia-KB954155-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB968389)(第 27 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB968389-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB952069)(第 28 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-WindowsMedia-KB952069-v2-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB973904)(第 29 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB973904-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB974318)(第 30 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB974318-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB974392)(第 31 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB974392-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB970430)(第 32 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB970430-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB972270)(第 33 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB972270-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB976662)(第 34 个更新，共 140 个)
+start /wait .\Patch\IE8-WindowsXP-KB976662-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB975560)(第 35 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB975560-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB975713)(第 36 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB975713-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB978706)(第 37 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB978706-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB977914)(第 38 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB977914-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB955759)(第 39 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB955759-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB978338)(第 40 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB978338-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB977816)(第 41 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB977816-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB979309)(第 42 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB979309-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB978542)(第 43 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB978542-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB979482)(第 44 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB979482-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB978695)(第 45 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-WindowsMedia-KB978695-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2229593)(第 46 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2229593-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2115168)(第 47 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2115168-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB981997)(第 48 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB981997-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB982665)(第 49 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB982665-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2347290)(第 50 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2347290-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2296011)(第 51 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2296011-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2378111)(第 52 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-WindowsMedia-KB2378111-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2387149)(第 53 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2387149-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB975558)(第 54 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-WindowsMedia-KB975558-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB979687)(第 55 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB979687-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB982132)(第 56 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB982132-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2345886)(第 57 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2345886-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2423089)(第 58 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2423089-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2443105)(第 59 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2443105-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2419632)(第 60 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2419632-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2393802)(第 61 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2393802-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2478960)(第 62 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2478960-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2478971)(第 63 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2478971-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2483185)(第 64 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2483185-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2479943)(第 65 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2479943-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2481109)(第 66 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2481109-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2485663)(第 67 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2485663-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2506212)(第 68 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2506212-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2508429)(第 69 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2508429-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2509553)(第 70 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2509553-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2510581)(第 71 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2510581-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB971029)(第 72 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB971029-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2535512)(第 73 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2535512-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2507938)(第 74 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2507938-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2566454)(第 75 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2566454-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2536276)(第 76 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2536276-v2-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2570947)(第 77 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2570947-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2544893)(第 78 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2544893-v2-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2564958)(第 79 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2564958-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2592799)(第 80 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2592799-x86-CHS.exe /U /Q /Z
+echo 正在安装 Internet Explorer 8 兼容性视图列表的更新 (KB2598845)(第 81 个更新，共 140 个)
+start /wait .\Patch\IE8-WindowsXP-KB2598845-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2619339)(第 82 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2619339-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2620712)(第 83 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2620712-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2584146)(第 84 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2584146-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2598479)(第 85 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2598479-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2603381)(第 86 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2603381-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2631813)(第 87 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2631813-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2585542)(第 88 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2585542-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2661637)(第 89 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2661637-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2653956)(第 90 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2653956-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2659262)(第 91 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2659262-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2676562)(第 92 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2676562-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2655992)(第 93 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2655992-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2686509)(第 94 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2686509-x86-CHS.exe /U /Q /Z	
+echo 正在安装 Windows XP 安全更新程序 (KB2691442)(第 95 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2691442-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2698365)(第 96 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2698365-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2712808)(第 97 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2712808-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2719985)(第 98 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2719985-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2705219)(第 99 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2705219-v2-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2723135)(第 100 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2723135-v2-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2749655)(第 101 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2749655-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2727528)(第 102 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2727528-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2770660)(第 103 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2770660-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2757638)(第 104 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2757638-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2780091)(第 105 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2780091-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2802968)(第 106 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2802968-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2807986)(第 107 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2807986-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2813345)(第 108 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2813345-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2820917)(第 109 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2820917-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2834886)(第 110 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2834886-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2850869)(第 111 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2850869-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2859537)(第 112 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2859537-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2803821)(第 113 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-WindowsMedia-KB2803821-v2-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2864063)(第 114 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2864063-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2876217)(第 115 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2876217-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2847311)(第 116 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2847311-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2862330)(第 117 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2862330-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2862335)(第 118 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2862335-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2862152)(第 119 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2862152-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2868626)(第 120 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2868626-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2876331)(第 121 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2876331-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2892075)(第 122 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2892075-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2893294)(第 123 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2893294-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2898715)(第 124 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2898715-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2904266)(第 125 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2904266-x86-CHS.exe /U /Q /Z
+echo 正在安装 Rvkroots (第 126 个更新，共 140 个)
+start /wait .\Patch\rvkroots.exe /Q
+echo 正在安装 Windows XP 安全更新程序 (KB2914368)(第 127 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2914368-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2909212)(第 128 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2909212-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2916036)(第 129 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2916036-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2936068)(第 130 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2936068-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2929961)(第 131 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2929961-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2930275)(第 132 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2930275-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2922229)(第 133 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2922229-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2964358)(第 134 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2964358-x86-Custom-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB4012598)(第 135 个更新，共 140 个)
+start /wait .\Patch\Windowsxp-KB4012598-x86-Custom-CHS.exe /U /Q /Z
+echo 正在安装 Internet Explorer 8 累积安全更新 (KB4018271)(第 136 个更新，共 140 个)
+start /wait .\Patch\IE8-WindowsXP-KB4018271-x86-Custom-CHS.exe /U /Q /Z
+echo 正在安装 Flash-Player-32.0.0.192 (第 137 个更新，共 140 个)
+start /wait .\Patch\install_flash_player_ax.exe /install
+echo 正在安装 Windows XP 安全更新程序 (KB4500331)(第 138 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB4500331-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB2900986)(第 139 个更新，共 140 个)
+start /wait .\Patch\WindowsXP-KB2900986-x86-CHS.exe /U /Q /Z
+echo 正在安装 Windows XP 安全更新程序 (KB890830)(第 140 个更新，共 140 个)
+start /wait .\Patch\Windows-KB890830-V5.14.exe /U /Q /Z
+
+ehco 安装完成，计算机将在60秒后进行重启...
+shutdown -r -f -t 60
+
+echo 如需立即重启请输入"N"并确认；
+echo 如需改为稍后手动重启，请输入"C"并确认；
+
+set /p  ID=
+if "%id%"=="N"  goto now
+if "%id%"=="C" goto cancle
+
+:now
+shutdown -a
+ping 127.0.0.1 -n 2 >nul
+shutdown -r -f -t 1
+goto end
+
+:cancle
+shutdown -a
+goto end
+
+:end
+exit
